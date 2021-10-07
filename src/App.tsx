f44ac910cbdeb630a40e16c4912c8ea73e1d96b9
Henrique Prado <henrique.mxnero@gmail.com>
2021-10-07 13:04:50 -0300

    Criação da funcionalidade de adicionar tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,38 @@
-import { useState } from 'react'
-import * as C from './App.styles';
-import {Item} from './types/Item';
-import { ListItem } from './components/ListItem/index';
+import { useState } from "react";
+import * as C from "./App.styles";
+import { Item } from "./types/Item";
+import { ListItem } from "./components/ListItem/index";
+import { AddArea } from "./components/AddArea";
 
 const App = () => {
-
   const [list, setList] = useState<Item[]>([
-    { id: 1, name: 'Comprar o pão na padaria', done: false},
-    { id: 2, name: 'Comprar um bolo na padaria', done: true}
+    { id: 1, name: "Comprar o pão na padaria", done: false },
+    { id: 2, name: "Comprar um bolo na padaria", done: true },
   ]);
 
-  return(
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
+  return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
 
-        {/** Área de adicionar nova tarefa */}
+        <AddArea onEnter={handleAddTask}/>
 
         {list.map((item, index) => (
           <ListItem key={index} item={item}></ListItem>
         ))}
-
       </C.Area>
     </C.Container>
   );
-}
+};
 
 export default App;
