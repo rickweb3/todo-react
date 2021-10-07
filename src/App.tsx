475f06a4010c1b88ad0cb1fc2eaa82c5ca01808f
Henrique Prado <henrique.mxnero@gmail.com>
2021-10-07 12:29:40 -0300

    Criação do componente de exibição das tarefas, com suas devidas estilizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import * as C from './App.styles';
+import {Item} from './types/Item';
+import { ListItem } from './components/ListItem/index';
 
 const App = () => {
+
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar o pão na padaria', done: false},
+    { id: 2, name: 'Comprar um bolo na padaria', done: true}
+  ]);
+
   return(
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        {/** Área de adicionar nova tarefa */}
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item}></ListItem>
+        ))}
+
       </C.Area>
     </C.Container>
   );
